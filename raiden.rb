272ea6384f90293ebecb0c5a6c460ab5791c7235
Ulrich Petri <ulo@ulo.pe>
2018-07-18 21:06:56 +0200

    Upgrade to 0.4.0

--- raiden.rb
+++ raiden.rb
@@ -1,14 +1,14 @@
 class Raiden < Formula
   desc "Raiden Network"
   homepage "https://github.com/cpurta/homebrew-raiden"
-  url "https://github.com/raiden-network/raiden/releases/download/v0.3.0/raiden-v0.3.0-macOS.zip"
-  version "v0.3.0"
-  sha256 "3f34d241f6b23d30d69c7bd25fa62de280e5f2bb34f9c08340f3e390e80e2809"
+  url "https://github.com/raiden-network/raiden/releases/download/v0.4.0/raiden-v0.4.0-macOS.zip"
+  version "v0.4.0"
+  sha256 "8728732aec2ac9b1a5885bf83866b2484c6d3cc1845950283fe3c08c196777b4"
 
   depends_on :macos => :el_capitan
 
   def install
-    system "mv", "raiden-0.3.0-macos", "raiden"
+    system "mv", "raiden-0.4.0-macos", "raiden"
     bin.install "raiden"
   end
 
