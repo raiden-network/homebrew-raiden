b184449739228b8b702e89418daca2c842ed3bd4
Ulrich Petri <ulo@ulo.pe>
2018-09-28 18:54:12 +0200

    Update to v0.11.0

--- raiden.rb
+++ raiden.rb
@@ -1,14 +1,14 @@
 class Raiden < Formula
   desc "Raiden Network"
   homepage "https://github.com/raiden-network/homebrew-raiden"
-  url "https://github.com/raiden-network/raiden/releases/download/v0.6.0/raiden-v0.6.0-macOS.zip"
-  version "v0.6.0"
-  sha256 "8caf1efb0f4d6a195a0170bd63d0a27ce8ae2704774fc9506595946687e75889"
+  url "https://github.com/raiden-network/raiden/releases/download/v0.11.0/raiden-v0.11.0-macOS.zip"
+  version "v0.11.0"
+  sha256 "6b609a318e6f68d2412bee5e327764858c18023c00f1efc7db65bab539f73e79"
 
   depends_on :macos => :el_capitan
 
   def install
-    mv "raiden-v0.6.0-macOS", "raiden"
+    mv "raiden-v0.11.0-macOS", "raiden"
     bin.install "raiden"
   end
 
