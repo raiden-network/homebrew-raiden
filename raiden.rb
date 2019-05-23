870edb03a75ff46d96e4b295a96cf9e27bfe25b2
Raiden Network <contact@raiden.network>
2019-05-23 08:32:30 +0000

    Update formula to v0.100.3

--- raiden.rb
+++ raiden.rb
@@ -1,14 +1,14 @@
 class Raiden < Formula
   desc "Raiden Network"
   homepage "https://github.com/raiden-network/homebrew-raiden"
-  url "https://github.com/raiden-network/raiden/releases/download/v0.100.3-rc7/raiden-v0.100.3-rc7-macOS-x86_64.zip"
-  version "v0.100.3-rc7"
+  url "https://github.com/raiden-network/raiden/releases/download/v0.100.3/raiden-v0.100.3-macOS-x86_64.zip"
+  version "v0.100.3"
   sha256 "90df67ab90d32b670dfda867fa3442b780b473e6f824859cd631f266758592ef"
 
   depends_on :macos => :el_capitan
 
   def install
-    mv "raiden-v0.100.3-rc7-macOS-x86_64", "raiden"
+    mv "raiden-v0.100.3-macOS-x86_64", "raiden"
     bin.install "raiden"
   end
 
