0bb077b52b01658f097af20775d25cf201cc67e0
Ulrich Petri <github@ulo.pe>
2020-04-09 15:51:54 +0200

    Fix broken formula

--- raiden.rb
+++ raiden.rb
@@ -1,14 +1,14 @@
 class Raiden < Formula
   desc "Raiden Network"
   homepage "https://github.com/raiden-network/homebrew-raiden"
-  url "https://github.com/raiden-network/raiden/releases/download/v0.200.0-rc5a0-dev1-dev0/raiden-v0.200.0-rc5a0-dev1-dev0-macOS-x86_64.zip"
-  version "v0.200.0-rc5a0-dev1-dev0"
+  url "https://github.com/raiden-network/raiden/releases/download/v0.200.0-rc5/raiden-v0.200.0-rc5-macOS-x86_64.zip"
+  version "v0.200.0-rc5"
   sha256 "90df67ab90d32b670dfda867fa3442b780b473e6f824859cd631f266758592ef"
 
   depends_on :macos => :el_capitan
 
   def install
-    mv "raiden-v0.200.0-rc5a0-dev1-dev0-macOS-x86_64", "raiden"
+    mv "raiden-v0.200.0-rc5-macOS-x86_64", "raiden"
     bin.install "raiden"
   end
 
