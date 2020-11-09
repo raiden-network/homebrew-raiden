3c2c643031ae01045d9292ae10688e0e7662034d
Raiden Network <contact@raiden.network>
2020-11-09 14:21:54 +0000

    Update formula to v1.2.0

--- raiden.rb
+++ raiden.rb
@@ -1,14 +1,14 @@
 class Raiden < Formula
   desc "Raiden Network"
   homepage "https://github.com/raiden-network/homebrew-raiden"
-  url "https://github.com/raiden-network/raiden/releases/download/v1.1.1/raiden-v1.1.1-macOS-x86_64.zip"
-  version "v1.1.1"
+  url "https://github.com/raiden-network/raiden/releases/download/v1.2.0/raiden-v1.2.0-macOS-x86_64.zip"
+  version "v1.2.0"
   sha256 "054a41b60d7112caea9b073917e9e094d7b9cecf8f81b1d26e9ad919406ef126"
 
   depends_on :macos => :el_capitan
 
   def install
-    mv "raiden-v1.1.1-macOS-x86_64", "raiden"
+    mv "raiden-v1.2.0-macOS-x86_64", "raiden"
     bin.install "raiden"
   end
 
